ece7fcacad504cdd9c13fcd0b1b8425b1c749d24
ConstantStar04 <101520486+ConstantStar04@users.noreply.github.com>
2022-12-20 09:37:51 +0100

    Update Rfid_doccia.ino

--- Rfid_doccia.ino
+++ Rfid_doccia.ino
@@ -1,17 +1,33 @@
-#include <LiquidCrystal_I2C.h>
-#include <SPI.h>
-#include <MFRC522.h>
-#include <Wire.h>
+#include <LiquidCrystal_I2C.h> #include <SPI.h>
+#include <MFRC522.h> #include <Wire.h>
 
-const byte SS_PIN = 10;
-const byte RST_PIN = 9;
+const byte SS_PIN = 10; const byte RST_PIN = 9;
 const byte lg = 2, lr = 3, lv = 4, lb = 5;
-const byte cs[4] = {148, 46, 203, 115};
-byte cl[4] = {NULL};
-byte vv = false, st = 0, pp = 0;
-unsigned long int dt, t1, dt2, t2,dtt,tt,dtc,tc = 0;
-const long int tp = 160000;//160000
+const byte cs[4] = {148, 46, 203, 115}; byte cl[4] = {NULL};
+byte vv = false, st = 0, pp = 0, x = 0; const long int tp = 160000;//160000
+unsigned long int dt, t1, dt2, t2, dt3, t3, dtt, tt, dtc, tc = 0;
+String rx = "";
+byte Edrop[8] = {
+  0b00100,
+  0b00100,
+  0b01010,
+  0b01010,
+  0b10001,
+  0b10001,
+  0b01010,
+  0b00100,
+};
 
+byte Fdrop[8] = {
+  0b00100,
+  0b00100,
+  0b01110,
+  0b01110,
+  0b11111,
+  0b11111,
+  0b01110,
+  0b00100,
+};
 MFRC522 mfrc522(SS_PIN, RST_PIN); LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup() {
@@ -19,52 +35,40 @@ void setup() {
   Serial.println("system start");
   SPI.begin(); mfrc522.PCD_Init();
   lcd.init(); lcd.init(); lcd.backlight();
-  pinMode(lg, 1);pinMode(lr, 1);pinMode(lv, 1);pinMode(lb, 1);
-  lcd.setCursor(0, 0);lcd.print("Doccia pronta");
-  lcd.setCursor(0, 1);lcd.print("avvicina tessera");
+  pinMode(lg, 1); pinMode(lr, 1); pinMode(lv, 1); pinMode(lb, 1);
+  lcd.createChar(0, Edrop);lcd.createChar(1, Fdrop);
+  lcd.setCursor(0, 0); lcd.print("Doccia pronta");
+  lcd.setCursor(0, 1); lcd.print("avvicina tessera");
   digitalWrite(lb, 1);
+
 }
 void loop() {
   tt = millis();
-  Serial.print(dtt);
-  Serial.print(" - ");
-  Serial.print((tp - dt) / 1000);
-  Serial.print(" - ");
+  Serial.print(dtt); Serial.print(" - "); Serial.print((tp - dt) / 1000); Serial.print(" - ");
   switch (st) {
     case 0:
-      Serial.print(pp);
-      Serial.print(" - ");
-      Serial.println("LETTURA");
+      Serial.print(pp); Serial.print(" - "); Serial.println("LETTURA");
       lettura();
       break;
     case 10:
-      Serial.print(pp);
-      Serial.print(" - ");
-      Serial.println("VERIFICA");
+      Serial.print(pp); Serial.print(" - "); Serial.println("VERIFICA");
       verifica();
       break;
     case 20:
       pp = 0;
-      
-      Serial.print(pp);
-      Serial.print(" - ");
-      Serial.println("LAVATI");
+      Serial.print(pp); Serial.print(" - "); Serial.println("LAVATI");
       doccia();
       break;
     case 150:
-      Serial.print(pp);
-      Serial.print(" - ");
-      Serial.println("DOVE SEI ANDATO?");
+      Serial.print(pp); Serial.print(" - "); Serial.println("DOVE SEI ANDATO?");
       psdoccia();
       break;
     case 200:
-      Serial.print(pp);
-      Serial.print(" - ");
-      Serial.println("RESET");
+      Serial.print(pp); Serial.print(" - "); Serial.println("RESET");
       rt();
       break;
   }
-  dtt = millis()-tt;
+  dtt = millis() - tt;
 }
 //####################################################################################################################################
 void lettura() {//0
@@ -74,10 +78,9 @@ void lettura() {//0
   Serial.print("UID tag :");
 
   for (byte i = 0; i < mfrc522.uid.size; i++) {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], DEC);
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "); Serial.print(mfrc522.uid.uidByte[i], DEC);
     cl[i] = (mfrc522.uid.uidByte[i]);
-  }Serial.println();
+  } Serial.println();
   st = 10;
 }
 //####################################################################################################################################
@@ -88,19 +91,12 @@ void verifica() {//10
     if (pp == 0 && cs[i] == cl[i]) {
       st = 20;
       lcd.clear();
-      lcd.print("lavati");
-      lcd.setCursor(0, 1);
-      lcd.print("tempo:");
+      lcd.setCursor(0, 0);lcd.print("tempo:");
       tc = t1 = millis();
     } else if (pp == 0 && cs[i] != cl[i]) {
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Tessera errata");
+      lcd.clear();lcd.setCursor(0, 0);lcd.print("Tessera errata");
       delay(800);
-      lcd.clear();
-      lcd.print("Doccia pronta");
-      lcd.setCursor(0, 1);
-      lcd.print("avvicina tessera");
+      lcd.clear();lcd.print("Doccia pronta");lcd.setCursor(0, 1);lcd.print("avvicina tessera");
       st = 0;
       return;
     }
@@ -110,10 +106,7 @@ void verifica() {//10
     } else if (pp == 10 && cs[i] != cl[i]) {
       st = 20;
       lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("lavati");
-      lcd.setCursor(0, 1);
-      lcd.print("tempo:");
+      lcd.setCursor(0, 0); lcd.print("tempo:");
       return;
     }
     //---------------------------------------------------------------------------------------------------------------------------------
@@ -121,29 +114,27 @@ void verifica() {//10
       st = 20;
       digitalWrite(lr, 0);
       lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("lavati");
-      lcd.setCursor(0, 1);
-      lcd.print("tempo:");
+      lcd.setCursor(0, 0); lcd.print("tempo:");
+      for (byte i = 0; i <= x; i++) {
+        lcd.setCursor(i, 1); lcd.write(1);
+        lcd.setCursor((i + 1), 1); lcd.write(0);
+        Serial.println("goccia");
+      }
       //t1 = millis();
     } else if (pp == 20 && cs[i] != cl[i]) {
       st = 150;
       lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Tessera errata");
+      lcd.setCursor(0, 0);lcd.print("Tessera errata");
       delay(800);
       lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("lavati");
-      lcd.setCursor(0, 1);
-      lcd.print("tempo:");
+      lcd.setCursor(0, 0);lcd.print("tempo:");
       return;
     }
   }
 }
 //####################################################################################################################################
 void doccia() {//20
-  dt = millis() - t1; dt2 = millis() - t2; dtc = millis() -tc;
+  dt = millis() - t1; dt2 = millis() - t2; dtc = millis() - tc; dt3 = millis() - t3;
   if (dt > 1000) {
     if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) { //carta non presente true [!false]
       Serial.println("verifica pausa");
@@ -151,18 +142,20 @@ void doccia() {//20
     }
   }
 
-  if(dtc > 3000){
-          lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("lavati");
-      lcd.setCursor(0, 1);
-      lcd.print("tempo:");
-      tc = millis();
-    }
-  
+  if ((tp - dt) / 1000 == 99 || (tp - dt) / 1000 == 10) {
+    lcd.setCursor(7, 0); lcd.print("   ");
+    tc = millis();
+  }
+
   if (dt < tp) {
     digitalWrite(lb, 0); digitalWrite(lv, 1);
-    lcd.setCursor(7, 1); lcd.print((tp - dt) / 1000);
+    lcd.setCursor(7, 0); lcd.print((tp - dt) / 1000);
+    if (dt3 > 10000) {
+      lcd.setCursor(x, 1); lcd.write(1);
+      lcd.setCursor((x + 1), 1); lcd.write(0);
+      x++;
+      t3 = millis();
+    }
     if (dt > (tp - 5000)) {
       if (dt2 > 500) {
         digitalWrite(lg, 1);
@@ -172,7 +165,9 @@ void doccia() {//20
         t2 = millis();
       }
     }
-  } else {st = 200;}
+  } else {
+    st = 200;
+  }
 }
 //####################################################################################################################################
 void psdoccia() {//150
@@ -180,19 +175,20 @@ void psdoccia() {//150
   if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) { //carta non presente true [!false]
     Serial.println("verifica ripressa");
     pp = 20; st = 0;
-    t1 = millis()- dt;
+    t1 = millis() - dt;
   }
   //lcd.clear();
-  lcd.setCursor(0, 0);lcd.print("Doccia in pausa");
-  lcd.setCursor(0, 1);lcd.print("avvicina tessera");
+  lcd.setCursor(0, 0); lcd.print("Doccia in pausa");
+  lcd.setCursor(0, 1); lcd.print("avvicina tessera");
 
 }
 //####################################################################################################################################
 void rt() {//200
   lcd.clear();
   t1 = millis();
-  st = 0;pp = 0;
-  lcd.setCursor(0, 0);lcd.print("Doccia pronta");
-  lcd.setCursor(0, 1);lcd.print("avvicina tessera");
-  digitalWrite(lb, 1);digitalWrite(lg, 0);digitalWrite(lr, 0);digitalWrite(lv, 0);
+  st = 0; pp = 0; x = 0;
+  lcd.setCursor(0, 0); lcd.print("Doccia pronta");
+  lcd.setCursor(0, 1); lcd.print("avvicina tessera");
+  digitalWrite(lb, 1); digitalWrite(lg, 0); digitalWrite(lr, 0); digitalWrite(lv, 0);
 }
+
